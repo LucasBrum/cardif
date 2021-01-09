a221d92f8772987f5eb4684c94568cfd45854ffb
Lucas Brum <brum.lucas@icloud.com>
2021-01-09 16:05:00 -0300

    Criando Repository de Funcionario

--- src/main/java/com/brum/cardif/respository/FuncionariRepository.java
+++ src/main/java/com/brum/cardif/respository/FuncionariRepository.java
@@ -1,5 +1,11 @@
 package com.brum.cardif.respository;
 
-public interface FuncionariRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brum.cardif.entity.Funcionario;
+
+@Repository
+public interface FuncionariRepository extends JpaRepository<Funcionario, Integer> {
 
 }
